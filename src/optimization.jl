170eeb08e7bd133ee190f2b84fa740072260b7a4
WhiffleFish <tylertronics@gmail.com>
2024-12-10 16:25:38 -0700

    support state distribution for `RegularizedMDPOptimizationProblem`

--- src/optimization.jl
+++ src/optimization.jl
@@ -1,15 +1,19 @@
 struct RegularizedMDPOptimizationProblem{M}
     pmdp::M
-    s_idx::Int64 # replace with μ0
+    μ0::SparseVector{Float64, Int64}
     w::Float64
 end
 
+RegularizedMDPOptimizationProblemO(pmdp; μ0=pmdp.μ0, w=1.0) = RegularizedMDPOptimizationProblem(pmdp, μ0, w)
+
 function value_and_grad(p::RegularizedMDPOptimizationProblem, θ)
-    (;s_idx, w, pmdp) = p
+    (;μ0, w, pmdp) = p
     frozen_mdp = MDPDesign.FrozenParameterizedTabularMDP(MDPDesign.ParameterizedTabularMDP(pmdp), θ)
     mdp_sol = MDPDesign.OptimizedMDPModel(frozen_mdp)
-    V = mdp_sol.V[s_idx]
-    ∇V = getindex.(MDPDesign.parameterization_gradient(mdp_sol),s_idx)
+    V = dot(μ0, mdp_sol.V)
+    ∇V = map(MDPDesign.parameterization_gradient(mdp_sol)) do dVdθi
+        dot(μ0, dVdθi)
+    end
     l = -V + 0.5 * w * dot(θ,θ)
     ∇l = -∇V .+ w .* θ
     return l, ∇l
