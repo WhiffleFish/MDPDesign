b456ffda35d62aa9f386e934a38d15e150c29fd0
WhiffleFish <tylertronics@gmail.com>
2024-12-10 15:21:39 -0700

    modify experiments for sigmoid

--- experiments/fully-parameterized-gw.jl
+++ experiments/fully-parameterized-gw.jl
@@ -6,8 +6,8 @@ using StaticArrays
 using MDPDesign.ParameterizedModels
 using Plots
 
-gw = FullyParameterizedGridWorld()
-θ = fill(0.7, 100)
+gw = FullyParameterizedGridWorld(discount=0.90)
+θ = fill(0.0, 100)
 
 frozen_mdp = MDPDesign.FrozenParameterizedTabularMDP(MDPDesign.ParameterizedTabularMDP(gw), θ)
 mdp_sol = MDPDesign.OptimizedMDPModel(frozen_mdp)
@@ -20,6 +20,12 @@ positions = vcat(
     [GWPos(x,y) for (x,y) in zip(ones(Int,length(2:3)) * 3, 2:3)]
 )
 
+annots = [
+    (4,6,Plots.text("-5")), 
+    (9,3,Plots.text("+10")),
+    (4,3,Plots.text("-10")),
+    (8,8,Plots.text("-10"))
+]
 
 max_grad = mapreduce(max, positions) do pos
     maximum(grad[stateindex(gw.mdp, pos)])
@@ -35,12 +41,13 @@ anim = @animate for pos ∈ positions
     for i ∈ eachindex(S)
         V_grid[S[i]...] = grad[s_idx][i]
     end
-    heatmap(V_grid',xticks=false,yticks=false, clims=(0, 5), cmap=:BuPu, legend=false, size=(700,700), primary=true)
-    scatter!(first.(r_pos), last.(r_pos), marker_z=r, cmap=cgrad([:red, :white, :green], [-10, 0, 10]), label="",legend=false, ms=20, primary=false)
+    V_max = maximum(V_grid)
+    heatmap(V_grid',xticks=false,yticks=false, clims=(0, V_max), cmap=:BuPu, size=(700,700), legend=false, primary=true, annotations=annots)
+    # scatter!(first.(r_pos), last.(r_pos), marker_z=r, cmap=cgrad([:red, :white, :green], [-10, 0, 10]), label="",legend=false, ms=20, primary=false)
     scatter!([first(pos)], [last(pos)], c=:white, ms=20, primary=false)
 end
 
-gif(anim, "fully-parameterized.gif", fps = 3)
+gif(anim, "fully-parameterized---.gif", fps = 3)
 
 
 pos2 = vcat(
@@ -59,4 +66,4 @@ anim2 = @animate for pos ∈ pos2
     # scatter!(first.(r_pos), last.(r_pos), marker_z=r, cmap=cgrad([:red, :white, :green], [-10, 0, 10]), label="",legend=false, ms=20, primary=false)
     scatter!([first(pos)], [last(pos)], c=:white, ms=20, primary=false)
 end
-gif(anim2, "fully-parameterized-lhs-paths.gif", fps = 3)
+gif(anim2, "fully-parameterized-lhs-paths-.gif", fps = 3)
--- experiments/uniformly-parameterized-gw.jl
+++ experiments/uniformly-parameterized-gw.jl
@@ -6,13 +6,18 @@ using StaticArrays
 using MDPDesign.ParameterizedModels
 using Plots
 
-gw = UniformlyParameterizedGridWorld()
-θ = [0.7]
+gw = UniformlyParameterizedGridWorld(discount=0.90)
+θ = [0.0]
 
 frozen_mdp = MDPDesign.FrozenParameterizedTabularMDP(MDPDesign.ParameterizedTabularMDP(gw), θ)
 mdp_sol = MDPDesign.OptimizedMDPModel(frozen_mdp)
 grad = only(MDPDesign.parameterization_gradient(mdp_sol))
 
 ∇V = MDPDesign.ParameterizedModels.vec2grid((10,10), grad)
-heatmap(∇V', cmap=:BuPu, xticks=false, yticks=false)
+heatmap(∇V', cmap=:BuPu, xticks=false, yticks=false, size=(800,700))
+savefig("uniformly-parameterized-grad.png")
 
+using Compose
+img = render(gw.mdp)
+savefig(img)
+img |> SVG("hello-world4.svg")
